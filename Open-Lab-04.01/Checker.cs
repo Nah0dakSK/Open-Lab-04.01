3cb93ebca6a11d24b96a055e212f08acdeaef885
Patrik Fatura <patofatura@gmail.com>
2020-10-30 15:19:01 +0100

    Done

--- Open-Lab-04.01/Checker.cs
+++ Open-Lab-04.01/Checker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._01
 {
@@ -6,7 +7,31 @@ namespace Open_Lab_04._01
     {
         public bool DoubleLetters(string str)
         {
-            throw new NotImplementedException();
+            var str = "hello llama";
+            var chars = str.ToCharArray();
+            Dictionary<char, int> charDictionary = new Dictionary<char, int>();
+            foreach(var ch in chars)
+            {
+                if(charDictionary.ContainsKey(ch)
+                {
+                charDictionary[ch] = charDictionary[ch] + 1;
+                }
+                else
+                {
+                charDictionary.Add(ch, 1);
+                }
+
+            }
+            var keys = new HashSet<char>(charDictionary.Keys);
+            foreach(var ch in keys)
+            {
+                if(charDictionary[ch] > 1)
+                {
+                 Console.WriteLine($"Character {ch} repeating {charDictionary[ch]} times.");
+                }
+
+            } 
+            Console.ReadKey();
         }
     }
 }
